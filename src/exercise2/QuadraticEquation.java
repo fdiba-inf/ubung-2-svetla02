6c33c6cba2a1884530306558d332c076a01e3f14
svetla02 <>
2021-10-21 10:24:59 +0000

    svetla

---
+++ src/exercise2/QuadraticEquation.java
@@ -0,0 +1,43 @@
+package exercise2;
+
+import java.util.Scanner;
+import java.lang.Math;
+public class QuadraticEquation {
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    double a = input.nextFloat();
+    double b = input.nextFloat();
+    double c = input.nextFloat();
+    double D = (b *b) - (4.0 * a * c);
+    double x1;
+    double x2;
+    double x;
+
+    if (a != 0) {
+      if (D >= 0) {
+        x1 = ((b * - 1) + Math.sqrt(D)) / (2 * a);
+        x2 = ((b * - 1) - Math.sqrt(D)) / (2 * a);
+        System.out.println(x1);
+        System.out.println(x2);
+      } else {
+        System.out.println("Imaginary values");
+      }
+    } else {
+      if (b != 0) {
+        x = (0 - c) / b;
+        System.out.println(x);
+      } else {
+        if (c != 0) {
+          System.out.println("No values");
+        } else {
+          System.out.println("Many values");
+        
+      
+        }
+      }
+        
+      }
+    }
+
+}
