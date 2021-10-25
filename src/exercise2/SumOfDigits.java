adde4662e387e456aa49de25052bc5cf1585ccd4
svetla02 <>
2021-10-25 13:14:58 +0000

    svetla

--- src/exercise2/SumOfDigits.java
+++ src/exercise2/SumOfDigits.java
@@ -5,19 +5,15 @@ import java.util.Scanner;
 public class  SumOfDigits {
 
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
-        int number = input.nextInt();
-
-        //456
-        
-        int number1 = number % 10; // 6
-        int number2 = (number / 10) % 10; // 5
-        int number3 = ((number / 10) / 10); // 4
-        
-        int sumOfDigits = number1 + number2 + number3;
-
-        System.out.println("Sum Of digits: " + sumOfDigits);
+        int number = input.nextInt();        
+        int number1 = number % 10;
+        int number2 = ((number/10)%10);
+        int number3 = ((number/10)/10);
+        int sum = number1 + number2 + number3;
+        System.out.println("Sum Of digits: " + sum);
   
         
     }
